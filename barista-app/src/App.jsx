00a60097809f6957162144ec4ff856f3636b93cd
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-26 21:56:00 -0400

    Add in-class progress

---
+++ barista-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BaristaForm from './components/BaristaForm'
+import RecipeChoices from './components/RecipeChoices'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="title-container">
+        <h1 className='title'>On My Grind</h1>
+        <p>So you think you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
+      <RecipeChoices />
+    </>
+  )
+}
+
+export default App
---
+++ barista-app/src/components/BaristaForm.jsx
@@ -0,0 +1,112 @@
+import React, {Component, useState} from "react";
+import RecipeChoices from "./RecipeChoices";
+import drinksJson from "./drinks.json"
+
+const BaristaForm = () => {
+    const [drink, setDrink] = useState('');
+    const [correctIngredients, setCorrectIngredients] = useState({});
+
+    const getNextDrink = () => {
+        let randomDrinkIndex = Math.floor(Math.random() * drinksJson.drinks.length);
+
+        setDrink(drinksJson.drinks[randomDrinkIndex].name);
+        setCorrectIngredients(drinksJson.drinks[randomDrinkIndex].ingredients);
+    }
+
+    const onNewDrink = () => {
+        setInputs({
+            'temperature': '',
+            'milk': '',
+            'syrup': '',
+            'blended': '' });
+        getNextDrink();
+    }
+    const onCheckAnswer = () => {
+    }
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+      });
+     
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
+  
+    return (
+        <div>
+            <h2>Hi, I'd like to order a: </h2>
+            <form>
+                <h3>Temperature</h3>
+                <div className="answer-space" >
+                {inputs["temperature"]} 
+                </div>
+                <RecipeChoices
+                handleChange={(e) => setInputs((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value,
+                }))}
+                label="temperature"
+                choices={ingredients["temperature"]}
+                checked={inputs["temperature"]}
+                />
+            
+
+                <h3>Milk</h3>
+                <div className="answer-space" >
+                {inputs["milk"]} 
+                </div>
+                <RecipeChoices
+                handleChange={(e) => setInputs((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value,
+                }))}
+                label="milk"
+                choices={ingredients["milk"]}
+                checked={inputs["milk"]}
+                />
+
+                <h3>Syrup</h3>
+                <div className="answer-space" >
+                {inputs["syrup"]} 
+                </div>
+                <RecipeChoices
+                handleChange={(e) => setInputs((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value,
+                }))}
+                label="syrup"
+                choices={ingredients["syrup"]}
+                checked={inputs["syrup"]}
+                />
+                
+                <h3>Blended</h3>
+                <div className="answer-space" >
+                {inputs["blended"]} 
+                </div>
+                <RecipeChoices
+                handleChange={(e) => setInputs((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value,
+                }))}
+                label="blended"
+                choices={ingredients["blended"]}
+                checked={inputs["blended"]}
+                />
+
+
+            </form>
+            <button className="button submit" type="new-drink-button">New Drink</button>
+            <button className="button submit" type="submit" onClick={onCheckAnswer}>Check Answer</button>
+        </div>
+    );
+  
+};
+
+export default BaristaForm;
+
